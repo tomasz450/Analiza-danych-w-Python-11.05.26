f523c7df7290448998998ebda3d79e2ad01afaf4
tomasz450 <tomasz.wilinski97@gmail.com>
2026-05-11 12:51:00 +0200

    m

---
+++ 1. Wstep. Python. Jupiter/Untitled-1.py
@@ -0,0 +1,49 @@
+# %%
+print('Hello b')
+
+# %%
+lista = [1,2,3,4,5]
+lista
+
+# %%
+for i in lista:
+    if i % 2 ==0:
+        print(i)
+
+# %%
+4*3
+
+# %%
+
+
+# %%
+1 & 1 #AND
+
+# %%
+1 | 0 # OR
+
+# %%
+lista
+
+# %%
+lista[0]
+
+# %%
+lista[-1]
+
+# %%
+lista
+
+# %%
+lista[1:3]
+
+# %%
+zmienna1 = "Napis" #string 
+
+# %%
+zmienna1 = 1 #integer
+type(zmienna1)
+d = {'key1':'item1','key2':'item2'}
+d['key1']
+
+
